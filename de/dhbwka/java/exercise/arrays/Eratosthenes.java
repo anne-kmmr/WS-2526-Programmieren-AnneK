db77b03caf5daa6bd01b2a85d6c0b1786d7987cc
anne-kmmr <annminecraft07@gmx.de>
2026-02-12 13:55:44 +0100

    worked on eratosthenes, but its not done by now

---
+++ de/dhbwka/java/exercise/arrays/Eratosthenes.java
@@ -0,0 +1,48 @@
+package de.dhbwka.java.exercise.arrays;
+import java.util.Scanner;
+
+//nicht fertig!
+
+public class Eratostenes {
+
+    private static void sieve(){
+        //Deklaration globaler Variablen & Arrays
+        Scanner inputScanner = new Scanner(System.in);
+        float primes[] = {}; //leer, siehe Anweisung
+
+        //Out- und Input
+        System.out.print("Welche Zahl möchtest du durch das Sieb des Eratostenes laufen lassen? ");
+        float n = inputScanner.nextFloat();
+        int arrayInput = (int) n;
+
+        //Größe des Arrays bestimmen
+        float[] sieve = new float[arrayInput];
+
+        //kleinste Zahl in sieves finden
+        int min = 0;
+        for (int i = 2; i < sieve.length; i++) {
+            if (sieve[i]) {
+                min = i;
+                break;
+            }
+        }
+
+        //Hinzufügen vom Minimum in primes
+        primes.add(min);
+
+        //i aus sieves löschen
+        for (int i = min; i < sieve.length; i += min) {
+            sieve[i] = false;
+        }
+
+        //Finalen Output
+        System.out.println("Hier alle Primzahlen zwischen 2 und n: ");
+        for (int i = 2; i < sieve.length; i++) {
+            System.out.print(sieve[i] + ", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        sieve();
+    }
+}
